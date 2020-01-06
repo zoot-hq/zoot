eeb50edb3c6ee73315ad5fc2a97637cc241442fe
Malka <Malka.in.red@gmail.com>
2020-01-06 06:43:59 -0500

    fix eula blocking signup button

--- components/Signup.js
+++ components/Signup.js
@@ -26,166 +26,167 @@ export default class SignupScreen extends React.Component {
     };
 
     return (
-      <KeyboardAvoidingView behavior='padding' style={{flex:1}}>
-        <View style={styles.container}>
-          <Text style={styles.title}>après</Text>
-          <View style={styles.field}>
-            <Text style={styles.text}>username</Text>
-            <TextInput
-              type="username"
-              returnKeyType="next"
-              onSubmitEditing={() => this.password.focus()}
-              autoCapitalize="none"
-              autoCorrect={false}
-              style={styles.input}
-              onChangeText={username => this.setState({ username })}
-              ref={input => (this.username = input)}
-              blurOnSubmit={false}
-            />
+      <View style={styles.container}>
+        <KeyboardAvoidingView behavior='padding' style={{flex:1}}>
+          <View style={styles.container}>
+            <Text style={styles.title}>après</Text>
+            <View style={styles.field}>
+              <Text style={styles.text}>username</Text>
+              <TextInput
+                type="username"
+                returnKeyType="next"
+                onSubmitEditing={() => this.password.focus()}
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.input}
+                onChangeText={username => this.setState({ username })}
+                ref={input => (this.username = input)}
+                blurOnSubmit={false}
+              />
+            </View>
+            {(this.state.error==='username is required.' || this.state.error==='username already taken.')&& (
+              <Text style={styles.error}>{this.state.error}</Text>
+            )}
+            <View style={styles.field}>
+              <Text style={styles.text}>email</Text>
+              <TextInput
+                type="email"
+                returnKeyType="next"
+                onSubmitEditing={() => this.username.focus()}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.input}
+                onChangeText={email => this.setState({ email })}
+                ref={input => (this.email = input)}
+                keyboardType="email-address"
+                blurOnSubmit={false}
+              />
+            </View>
+            {(this.state.error==='The email address is badly formatted.' ||
+              this.state.error==='The email address is already in use by another account.')&& (
+              <Text style={styles.error}>{this.state.error}</Text>
+            )}
+            <View style={styles.field}>
+              <Text style={styles.text}>password</Text>
+              <TextInput
+                returnKeyType="next"
+                secureTextEntry
+                onSubmitEditing={() => this.birthday.focus()}
+                style={styles.input}
+                onChangeText={password => this.setState({ password })}
+                ref={input => (this.password = input)}
+                blurOnSubmit={false}
+              />
+            </View>
+            {(this.state.error==='The password must be 6 characters long or more.' ||
+              this.state.error==='Password should be at least 6 characters') && (
+              <Text style={styles.error}>{this.state.error}</Text>
+            )}
+            <View style={styles.field}>
+              <Text style={styles.text}>birthday (ddmmyyyy)</Text>
+              <TextInput
+                type="birthday"
+                returnKeyType="next"
+                onSubmitEditing={() => this.city.focus()}
+                autoCorrect={false}
+                style={styles.input}
+                onChangeText={birthday => this.setState({ birthday })}
+                ref={input => (this.birthday = input)}
+                keyboardType="number-pad"
+                blurOnSubmit={false}
+              />
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.text}>city</Text>
+              <TextInput
+                type="city"
+                returnKeyType="next"
+                // onSubmitEditing={() => this.children.focus()}
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.input}
+                onChangeText={city => this.setState({ city })}
+                ref={input => (this.city = input)}
+                blurOnSubmit={false}
+              />
+            </View>
+            {/* <View style={styles.field}>
+              <Text style={styles.text}>children (number)</Text>
+              <TextInput
+                type="children"
+                returnKeyType="next"
+                onSubmitEditing={() => this.monthsPostPartum.focus()}
+                keyboardType="numeric"
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.input}
+                onChangeText={children => this.setState({ children })}
+                ref={input => (this.children = input)}
+                keyboardType="number-pad"
+                blurOnSubmit={false}
+              />
+            </View> */}
+            {/* <View style={styles.field}>
+              <Text style={styles.text}>months post partum</Text>
+              <TextInput
+                type="monthsPostPartum"
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.input}
+                onChangeText={monthsPostPartum => this.setState({ monthsPostPartum })}
+                keyboardType="number-pad"
+                ref={input => (this.monthsPostPartum = input)}
+              />
+            </View> */}
+            <TouchableOpacity
+              style={styles.buttonContainer}
+              onPress={async () => {
+
+                // ensure a username is chosen
+                if (!this.state.username.length) {
+                  this.setState({ error: 'username is required.'})
+                  return
+                }
+
+                // sign up a user
+                const status = await Fire.shared.signup
+                (
+                  this.state.email,
+                  this.state.password,
+                  this.state.username,
+                  this.state.birthday,
+                  this.state.city,
+                  this.state.children,
+                  this.state.monthsPostPartum
+                )
+
+                // if error occured, put it on state
+                if (status) {
+                  console.log('error message: ', status)
+                  this.setState({ error: status.message})
+                }
+
+                // if everything is good, navigate into the app
+                else {
+                  this.props.navigation.navigate('WelcomePage')
+                }
+              }}
+            >
+              <Text style={styles.buttonText}>sign me up!</Text>
+            </TouchableOpacity>
           </View>
-          {(this.state.error==='username is required.' || this.state.error==='username already taken.')&& (
-            <Text style={styles.error}>{this.state.error}</Text>
-          )}
-          <View style={styles.field}>
-            <Text style={styles.text}>email</Text>
-            <TextInput
-              type="email"
-              returnKeyType="next"
-              onSubmitEditing={() => this.username.focus()}
-              keyboardType="email-address"
-              autoCapitalize="none"
-              autoCorrect={false}
-              style={styles.input}
-              onChangeText={email => this.setState({ email })}
-              ref={input => (this.email = input)}
-              keyboardType="email-address"
-              blurOnSubmit={false}
-            />
+
+          <View style={styles.eula}>
+            <Text style={styles.eulaText}>By proceeding with signing in and clicking 'Sign me up!', you agree to our terms as listed in our</Text>
+            <Text style={styles.link}
+                onPress={() => Linking.openURL('http://gist.githubusercontent.com/lisjak/5196333df14d1f708563804a885a1b66/raw/8ed9e754f8cbddd156472f02487ef8bcf4ef52ff/apres-eula')}>
+                End-User License Agreement (EULA) of Après.
+            </Text>
           </View>
-          {(this.state.error==='The email address is badly formatted.' ||
-            this.state.error==='The email address is already in use by another account.')&& (
-            <Text style={styles.error}>{this.state.error}</Text>
-          )}
-          <View style={styles.field}>
-            <Text style={styles.text}>password</Text>
-            <TextInput
-              returnKeyType="next"
-              secureTextEntry
-              onSubmitEditing={() => this.birthday.focus()}
-              style={styles.input}
-              onChangeText={password => this.setState({ password })}
-              ref={input => (this.password = input)}
-              blurOnSubmit={false}
-            />
-          </View>
-          {(this.state.error==='The password must be 6 characters long or more.' ||
-            this.state.error==='Password should be at least 6 characters') && (
-            <Text style={styles.error}>{this.state.error}</Text>
-          )}
-          <View style={styles.field}>
-            <Text style={styles.text}>birthday (ddmmyyyy)</Text>
-            <TextInput
-              type="birthday"
-              returnKeyType="next"
-              onSubmitEditing={() => this.city.focus()}
-              autoCorrect={false}
-              style={styles.input}
-              onChangeText={birthday => this.setState({ birthday })}
-              ref={input => (this.birthday = input)}
-              keyboardType="number-pad"
-              blurOnSubmit={false}
-            />
-          </View>
-          <View style={styles.field}>
-            <Text style={styles.text}>city</Text>
-            <TextInput
-              type="city"
-              returnKeyType="next"
-              // onSubmitEditing={() => this.children.focus()}
-              autoCapitalize="none"
-              autoCorrect={false}
-              style={styles.input}
-              onChangeText={city => this.setState({ city })}
-              ref={input => (this.city = input)}
-              blurOnSubmit={false}
-            />
-          </View>
-          {/* <View style={styles.field}>
-            <Text style={styles.text}>children (number)</Text>
-            <TextInput
-              type="children"
-              returnKeyType="next"
-              onSubmitEditing={() => this.monthsPostPartum.focus()}
-              keyboardType="numeric"
-              autoCapitalize="none"
-              autoCorrect={false}
-              style={styles.input}
-              onChangeText={children => this.setState({ children })}
-              ref={input => (this.children = input)}
-              keyboardType="number-pad"
-              blurOnSubmit={false}
-            />
-          </View> */}
-          {/* <View style={styles.field}>
-            <Text style={styles.text}>months post partum</Text>
-            <TextInput
-              type="monthsPostPartum"
-              autoCapitalize="none"
-              autoCorrect={false}
-              style={styles.input}
-              onChangeText={monthsPostPartum => this.setState({ monthsPostPartum })}
-              keyboardType="number-pad"
-              ref={input => (this.monthsPostPartum = input)}
-            />
-          </View> */}
-          <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={async () => {
-
-              // ensure a username is chosen
-              if (!this.state.username.length) {
-                this.setState({ error: 'username is required.'})
-                return
-              }
-
-              // sign up a user
-              const status = await Fire.shared.signup
-              (
-                this.state.email,
-                this.state.password,
-                this.state.username,
-                this.state.birthday,
-                this.state.city,
-                this.state.children,
-                this.state.monthsPostPartum
-              )
-
-              // if error occured, put it on state
-              if (status) {
-                console.log('error message: ', status)
-                this.setState({ error: status.message})
-              }
-
-              // if everything is good, navigate into the app
-              else {
-                this.props.navigation.navigate('WelcomePage')
-              }
-            }}
-          >
-            <Text style={styles.buttonText}>sign me up!</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.eula}>
-                <Text style={styles.eulaText}>By proceeding with signing in and clicking 'Sign me up!', you agree to our terms as listed in our</Text>
-                <Text style={styles.link}
-            onPress={() => Linking.openURL('http://gist.githubusercontent.com/lisjak/5196333df14d1f708563804a885a1b66/raw/8ed9e754f8cbddd156472f02487ef8bcf4ef52ff/apres-eula')}>
-        End-User License Agreement (EULA) of Après.
-        </Text>
-        </View>
-
-
-      </KeyboardAvoidingView>
+          
+        </KeyboardAvoidingView>
+      </View>
     );
   }
 }
@@ -201,8 +202,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'white',
     textAlign: 'center',
-    flex: 0,
-    paddingBottom: 50,
+    paddingBottom: 10,
+    marginTop: 30
   },
   eulaText: {
     display: 'flex',
@@ -212,6 +213,7 @@ const styles = StyleSheet.create({
     marginLeft: 50,
     letterSpacing: 1,
     fontFamily: "Futura-Light",
+    marginTop: 10
   },
   link: {
     color: 'blue',
@@ -228,7 +230,7 @@ const styles = StyleSheet.create({
     fontSize: 60,
     fontWeight: '700',
     textAlign: 'center',
-    marginBottom: 30,
+    marginBottom: 20,
     marginTop: 80,
     fontFamily: "CormorantGaramond-Light"
   },
@@ -251,10 +253,11 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderWidth: 1,
     paddingVertical: 5,
-    marginBottom: 15,
+    marginBottom: 30,
     marginTop: 30,
     marginRight: 50,
     marginLeft: 50,
+    paddingBottom: 10
   },
   buttonText: {
     textAlign: 'center',
