a1b0350f393d2bd658c1005d99edbaaed33492fb
Malka <Malka.in.red@gmail.com>
2020-04-02 08:16:56 -0400

    center people icon

--- components/ChatList.js
+++ components/ChatList.js
@@ -196,6 +196,7 @@ const styles = StyleSheet.create({
   singleChatView: {
     display: 'flex', 
     flexDirection: 'row', 
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center'
   }
 });
