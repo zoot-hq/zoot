83596c14f25113846b086ed59069a7be0e99bdf1
lkobetz <larissa.kobetz@gmail.com>
2020-04-20 15:54:40 -0400

    user can update username on user page

--- components/UserPage.js
+++ components/UserPage.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
 import Modal from 'react-native-modal';
 import Fire from '../Fire';
 import firebase from 'firebase';
@@ -10,14 +16,29 @@ export default class UserPage extends Component {
     this.state = {
       userNameModal: false,
       emailModal: false,
-      passwordModal: false
+      passwordModal: false,
+      newUsername: '',
+      error: false
     };
-    const user = firebase.auth().currentUser;
+    this.user = firebase.auth().currentUser;
   }
   renderModal(type) {
     const stateObj = {};
     stateObj[type] = true;
     this.setState(stateObj);
+  }
+  updateUsername() {
+    if (this.state.newUsername) {
+      this.user
+        .updateProfile({
+          displayName: this.state.newUsername
+        })
+        .then(() => this.setState({newUsername: ''}))
+        .catch((error) => this.setState({error: error}))
+        .then(this.setState({error: false, userNameModal: false}));
+    } else {
+      this.setState({error: true});
+    }
   }
   render() {
     return (
@@ -33,7 +54,44 @@ export default class UserPage extends Component {
           <Text style={styles.userInfo}>Update username?</Text>
         </TouchableOpacity>
         <Modal isVisible={this.state.userNameModal}>
-          <Text>Update username</Text>
+          <View style={styles.modal}>
+            <Text style={styles.modalTitle}>Update username</Text>
+            {this.state.error ? (
+              <Text style={styles.modalText}>
+                There was an error updating your username. Please try again.
+              </Text>
+            ) : (
+              <Text style={styles.modalText}>
+                Type your new desired username below.
+              </Text>
+            )}
+            <TextInput
+              placeholder="New username"
+              placeholderTextColor="#bfbfbf"
+              autoCapitalize="none"
+              autoCorrect={false}
+              style={styles.input}
+              onChangeText={(newUsername) => this.setState({newUsername})}
+            />
+            <View style={styles.modalButtonsContainer}>
+              <TouchableOpacity
+                style={{width: 150}}
+                onPress={() => this.setState({userNameModal: false})}
+              >
+                <Text style={styles.modalButtonCancel}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  width: 150,
+                  borderLeftWidth: 1,
+                  borderLeftColor: 'gray'
+                }}
+                onPress={() => this.updateUsername()}
+              >
+                <Text style={styles.modalButtonSave}>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </Modal>
         <Text></Text>
         <Text style={styles.userInfo}>email: {Fire.shared.email()}</Text>
@@ -121,5 +179,65 @@ const styles = StyleSheet.create({
     fontFamily: 'Futura-Light',
     fontSize: 25,
     textAlign: 'center'
+  },
+  modal: {
+    backgroundColor: 'whitesmoke',
+    borderRadius: 10,
+    width: 300,
+    alignSelf: 'center'
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: '500',
+    textAlign: 'center',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20
+  },
+  modalText: {
+    fontSize: 15,
+    textAlign: 'center',
+    marginLeft: 20,
+    marginRight: 20
+  },
+  input: {
+    borderBottomWidth: 0.5,
+    borderBottomColor: 'gray',
+    borderLeftWidth: 0.5,
+    borderLeftColor: 'gray',
+    borderRightWidth: 0.5,
+    borderRightColor: 'gray',
+    borderTopWidth: 0.5,
+    borderTopColor: 'gray',
+    backgroundColor: 'white',
+    padding: 5,
+    marginVertical: 20,
+    flexGrow: 1,
+    textAlignVertical: 'bottom',
+    marginLeft: 15,
+    marginRight: 15,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+    borderBottomLeftRadius: 5
+  },
+  modalButtonsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderTopColor: 'gray'
+  },
+  modalButtonCancel: {
+    color: '#0073e6',
+    fontSize: 20,
+    textAlign: 'center',
+    marginVertical: 10
+  },
+  modalButtonSave: {
+    color: '#0073e6',
+    fontSize: 20,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginVertical: 10
   }
 });
